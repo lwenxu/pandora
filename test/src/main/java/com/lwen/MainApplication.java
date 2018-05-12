e7d63143b1ede776326e206e2867788ef32878f9
lwen <1405766743@qq.com>
2018-05-12 22:34:07 +0800

    一个简单的crud完成！

---
+++ test/src/main/java/com/lwen/MainApplication.java
@@ -0,0 +1,22 @@
+package com.lwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+@EnableCaching
+@ComponentScan(basePackages= {"com.lwen","com.ibeetl.admin"})
+public class MainApplication  extends SpringBootServletInitializer implements WebApplicationInitializer {
+	
+    public static void main(String[] args) {
+    	
+    	SpringApplication.run(MainApplication.class, args);
+    }
+
+
+
+}	
